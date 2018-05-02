feb0b9bea3b06b9c3e311966160e8358bb55289c
francoMG <francomolocheg99@gmail.com>
2018-05-02 12:40:01 -0500

    Update Events.h

--- Trabajo_Parcial/Trabajo_Parcial/Events.h
+++ Trabajo_Parcial/Trabajo_Parcial/Events.h
@@ -1,6 +1,6 @@
 #pragma once
 
-void Events(unsigned char *memory, int memory_size, map<string, unsigned char> &vars) {
+void Events(unsigned char *memory, int memory_size, vector<var> &vars) {
 
 	int end = false;
 	string input;
@@ -11,22 +11,22 @@ void Events(unsigned char *memory, int memory_size, map<string, unsigned char> &
 
 		cout << "\n 1- VER SECCION DE MEMORIA"
 			<< "\n 2- INGRESAR VALOR EN LA MEMORIA"
+			<< "\n 3- DECLARAR ARREGLO"
+			<< "\n 4- DECLARAR PUNTERO"
 			<< "\n %- SALIR"
 			<< "\n\n >>> ";
 
 		cin >> input;
 		
-		//Ver seccion de memoria
-		if (input == "1") {
+		if (input == "1") { //Ver seccion de memoria
 
 			cout << "\nIngrese la direccion (hex): ";
 
 			cin >> input;
 			seeSection(input, memory, memory_size);
 		}
-
-		//Ingresar valor en la memoria
-		else if (input[0] == '2') {
+		
+		else if (input[0] == '2') { //Ingresar valor en la memoria
 			
 			int tp, dir, val;
 			cout << "\nSeleccione el tipo de dato:\n"
@@ -36,6 +36,7 @@ void Events(unsigned char *memory, int memory_size, map<string, unsigned char> &
 				<<"\n 4- FLOAT"
 				<<"\n 5- BOOL"
 				<< "\n\n >>> ";
+			
 			cin >> tp;
 			
 			cout << "\nIngrese el valor: "
@@ -45,17 +46,83 @@ void Events(unsigned char *memory, int memory_size, map<string, unsigned char> &
 			cout << "\nIngrese la direccion (hex): "
 				<< "\n\n >>> ";
 			cin >> hex >> dir >> dec;
-
-			writeVar(memory, dir, (unsigned char *)&val,
-				((tp == 1) * sizeof(int)) + ((tp == 2) * sizeof(char)) + ((tp == 3) * sizeof(double)) +
-				((tp == 4) * sizeof(float)) + ((tp == 5) * sizeof(bool))
-			);
+			
+			tp = ((tp == 1) * sizeof(int)) + ((tp == 2) * sizeof(char)) + ((tp == 3) * sizeof(double)) +
+				((tp == 4) * sizeof(float)) + ((tp == 5) * sizeof(bool));
+			
+			writeVar(memory, dir, (unsigned char *)&val, tp);
 		}
 
-		//Salir
-		else {
+		else if (input[0] == '3') { //Declarar arreglo
+			
+			string nv;
+			int tp, ta, val;
+
+			cout << "\nSeleccione el tipo de dato:\n"
+				<< "\n 1- INT"
+				<< "\n 2- CHAR"
+				<< "\n 3- DOUBLE"
+				<< "\n 4- FLOAT"
+				<< "\n 5- BOOL"
+				<< "\n\n >>> ";
+			
+			cin >> tp;
+
+			cout << "\n Ingrese el nombre de la varible: "
+				<< "\n\n >>>";
+			cin >> nv;
+
+			cout << "\n Ingrese el tamanio del arreglo: "
+				<< "\n\n >>>";
+			cin >> ta;
+			
+			tp = ((tp == 1) * sizeof(int)) + ((tp == 2) * sizeof(char)) + ((tp == 3) * sizeof(double)) +
+				((tp == 4) * sizeof(float)) + ((tp == 5) * sizeof(bool));
+			
+			for (int i = 0; i < ta; i++) {
+				
+				vars.push_back(var(nv+to_string(i)), tp,selfAssign(vars));///////////////////////////////
+				
+				cout << "\nIngrese el valor " << i << " del arreglo: "
+					<< "\n\n >>> ";
+				
+				cin >> val;
+				writeVar(memory, vars[vars.size()-1].dir, (unsigned char *)&val, tp);
+			}
+		}
+
+		else if (input[0] == '4') { //Declarar arreglo
+			
+			string nv;
+			int tp, val;
+			
+			cout << "\n Ingrese el nombre de la varible: "
+				<< "\n\n >>>";
+			cin >> nv;
+
+			cout << "\nSeleccione el tipo de dato:\n"
+				<< "\n 1- INT"
+				<< "\n 2- CHAR"
+				<< "\n 3- DOUBLE"
+				<< "\n 4- FLOAT"
+				<< "\n 5- BOOL"
+				<< "\n\n >>> ";
+			cin >> tp;
+
+			cout << "\nIngrese el valor: "
+				<< "\n\n >>> ";
+			cin >> val;
+		}
+		
+		else { //Salir
 
 			end = true;
 		}
 	}
 }
+
+int selfAssign(vector<var> &vars){
+	
+	return vars[vars.size()-1].dir + vars[vars.size()-1].tp;
+}
+
